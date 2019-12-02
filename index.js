e1baaf932cc4b3bdbc630546c72c5e74fffc46a8
Chanon <ccionflare090290@gmail.com>
2019-12-02 14:31:01 +0700

    add identifier to socket-io sv

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // client.js
 var io = require('socket.io-client');
-//var socket = io('http://localhost:3000/');
-var socket = io('https://my-socketio.herokuapp.com/');
+var socket = io('http://localhost:3000/');
+//var socket = io('https://my-socketio.herokuapp.com/');
 
 socket.on('connect', function () {
   socket.send('hi');
 
   socket.on('message', function (msg) {
-    console.log(msg);
+    var new_msg = msg +" naja !!"
+    console.log(new_msg);
   });
-  socket.on('chat message', function (msg) {
-    console.log(msg);
-  });
+  
 });
+
+socket.on('chat message', function (msg) {
+  var new_msg = msg +" naja !!"
+  console.log(new_msg);
+});
+
+interval = setInterval(function () { //#
+  //console.log("จุ๊บๆ");
+  socket.emit ('chat message', "FROM BOT: อิอิ");
+}, 2000);
